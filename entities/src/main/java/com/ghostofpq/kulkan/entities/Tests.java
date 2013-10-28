3842363186fbe14d31d819db9a03ab4ea137b67c
Ghostofpq <ghostofpq@gmail.com>
2013-10-28 13:26:26 +0100

    besoin de rien en fait

--- entities/src/main/java/com/ghostofpq/kulkan/entities/Tests.java
+++ entities/src/main/java/com/ghostofpq/kulkan/entities/Tests.java
@@ -1,29 +1,16 @@
 package com.ghostofpq.kulkan.entities;
 
 import com.ghostofpq.kulkan.entities.character.Player;
-import com.ghostofpq.kulkan.entities.character.PlayerRepository;
 import com.mongodb.Mongo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-import java.net.UnknownHostException;
 
 @Configuration
 @EnableMongoRepositories
 public class Tests {
-
-    @Autowired
-    PlayerRepository playerRepository;
-
-    @Bean
-    Mongo mongo() throws UnknownHostException {
-        return new Mongo("localhost");
-    }
-
     public static void main(String[] args) throws Exception {
         MongoOperations mongoOps = new MongoTemplate(new Mongo(), "kulkan");
         Player p1 = new Player("Bob");
