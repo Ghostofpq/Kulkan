a0e9f08a4eb783439289457142f8987f9866fd16
Ghostofpq <ghostofpq@gmail.com>
2013-11-06 14:24:32 +0100

    team management

---
+++ client/src/main/java/com/ghostofpq/kulkan/client/scenes/TeamManagementScene.java
@@ -0,0 +1,27 @@
+package com.ghostofpq.kulkan.client.scenes;
+
+public class TeamManagementScene implements Scene {
+    @Override
+    public void init() {
+    }
+
+    @Override
+    public void update(long deltaTime) {
+    }
+
+    @Override
+    public void render() {
+    }
+
+    @Override
+    public void manageInput() {
+    }
+
+    @Override
+    public void closeConnections() {
+    }
+
+    @Override
+    public void receiveMessage() {
+    }
+}
