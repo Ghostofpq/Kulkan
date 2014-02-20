fbbad40ed5ed163e17dae5b75177d54b2aed3330
Ghostofpq <ghostofpq@gmail.com>
2014-02-20 17:22:34 +0100

    di ptit coumparateurs ouhlala

--- entities/src/main/java/com/ghostofpq/kulkan/entities/inventory/item/Item.java
+++ entities/src/main/java/com/ghostofpq/kulkan/entities/inventory/item/Item.java
@@ -5,6 +5,7 @@ import com.ghostofpq.kulkan.entities.characteristics.SecondaryCharacteristics;
 import com.ghostofpq.kulkan.entities.job.JobType;
 
 import java.io.Serializable;
+import java.util.Comparator;
 import java.util.List;
 
 public abstract class Item implements Serializable {
@@ -16,6 +17,67 @@ public abstract class Item implements Serializable {
     private List<JobType> authorizedJobs;
     private ItemType itemType;
     private int price;
+
+    public static Comparator<Item> nameComparatorAlpha = new Comparator<Item>() {
+        @Override
+        public int compare(Item o1, Item o2) {
+            int res = String.CASE_INSENSITIVE_ORDER.compare(o1.getName(), o2.getName());
+            return res;
+        }
+    };
+
+    public static Comparator<Item> nameComparatorAntiAlpha = new Comparator<Item>() {
+        @Override
+        public int compare(Item o1, Item o2) {
+            int res = -String.CASE_INSENSITIVE_ORDER.compare(o1.getName(), o2.getName());
+            return res;
+        }
+    };
+
+    public static Comparator<Item> typeComparatorAlpha = new Comparator<Item>() {
+        @Override
+        public int compare(Item o1, Item o2) {
+            int res = String.CASE_INSENSITIVE_ORDER.compare(o1.getItemType().toString(), o2.getItemType().toString());
+            if (res == 0) {
+                res = o1.getName().compareTo(o2.getName());
+            }
+            return res;
+        }
+    };
+
+    public static Comparator<Item> typeComparatorAntiAlpha = new Comparator<Item>() {
+        @Override
+        public int compare(Item o1, Item o2) {
+            int res = -String.CASE_INSENSITIVE_ORDER.compare(o1.getItemType().toString(), o2.getItemType().toString());
+            if (res == 0) {
+                res = o1.getName().compareTo(o2.getName());
+            }
+            return res;
+        }
+    };
+
+    public static Comparator<Item> priceComparatorIncr = new Comparator<Item>() {
+        @Override
+        public int compare(Item o1, Item o2) {
+            int res = Integer.compare(o1.getPrice(), o2.getPrice());
+            if (res == 0) {
+                res = o1.getName().compareTo(o2.getName());
+            }
+            return res;
+        }
+    };
+
+    public static Comparator<Item> priceComparatorDesc = new Comparator<Item>() {
+        @Override
+        public int compare(Item o1, Item o2) {
+            int res = -Integer.compare(o1.getPrice(), o2.getPrice());
+            if (res == 0) {
+                res = o1.getName().compareTo(o2.getName());
+            }
+            return res;
+        }
+    };
+
 
     protected Item(String itemID, String name, String description, PrimaryCharacteristics primaryCharacteristics, SecondaryCharacteristics secondaryCharacteristics, List<JobType> authorizedJobs, ItemType itemType, int price) {
         this.itemID = itemID;
